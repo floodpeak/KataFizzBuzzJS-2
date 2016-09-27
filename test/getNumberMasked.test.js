677efc0b80e0a03d1d89c95b4325fa9b0c88241f
floodpeak <hongfengzhou@yahoo.com>
2016-09-28 01:12:33 +0800

    add unit test for 2
    not pass of course

--- test/getNumberMasked.test.js
+++ test/getNumberMasked.test.js
@@ -6,4 +6,7 @@ describe('getNumberMasked',function(){
   it('should get 1 for 1',function(){
     expect(getNumberMasked(1)).to.be.equal(1)
   })
+  it('should get 2 for 2',function(){
+    expect(getNumberMasked(2)).to.be.equal(2)
+  })
 })
